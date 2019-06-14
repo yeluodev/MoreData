6d4cd0347263ba34d3f689d09e9949e65efe62b5
yeluodev1226 <yeluodev1226@163.com>
2019-06-14 09:55:42 +0800

    fix bug，cookie随机切换导致与账号相关的功能一定几率失败，如发帖、传图、调仓等

--- src/main/java/club/moredata/api/ApiManager.java
+++ src/main/java/club/moredata/api/ApiManager.java
@@ -55,18 +55,6 @@ public class ApiManager {
         return instance;
     }
 
-    private Headers commonHeaders() {
-        Headers.Builder headersBuilder = new Headers.Builder();
-        headersBuilder.add("Host", "api.xueqiu.com");
-        headersBuilder.add("Accept", "application/json");
-        headersBuilder.add("Cookie", "xq_a_token=26e7dfb7dd01d9ce96e44aa198c6518fd2ca30d6;u=9084578148");
-        headersBuilder.add("User-Agent", "Xueqiu iPhone 11.19.1");
-        headersBuilder.add("Accept-Language", "zh-Hans-CN;q=1");
-        headersBuilder.add("Connection", "keep-alive");
-
-        return headersBuilder.build();
-    }
-
     /**
      * 获取雪球用户信息
      *
@@ -118,8 +106,6 @@ public class ApiManager {
     public void fetchCubeDetail(String symbol, ApiCallback callback) {
         String params = "?mix_rebalancing=true&ret_last_buy_rb_gid=true&symbol=" + symbol;
         Request request = new Request.Builder()
-//                .addHeader("Cookie","xq_a_token=df8524b38974fce13d20b7ee39af90871c03be91;u=5414038194")
-//                .addHeader("Cookie","xq_a_token=0aebf2e32b55e41f30f0042f3e556b5e26206173;u=411560226561917")
                 .url(CUBES_DETAIL + params)
                 .build();
         client.newCall(request).enqueue(callback);
@@ -128,8 +114,6 @@ public class ApiManager {
     public Response fetchCubeDetail(String symbol) {
         String params = "?mix_rebalancing=true&ret_last_buy_rb_gid=true&symbol=" + symbol;
         Request request = new Request.Builder()
-//                .addHeader("Cookie","xq_a_token=df8524b38974fce13d20b7ee39af90871c03be91;u=5414038194")
-//                .addHeader("Cookie","xq_a_token=0aebf2e32b55e41f30f0042f3e556b5e26206173;u=411560226561917")
                 .url(CUBES_DETAIL + params)
                 .build();
         try {
@@ -166,14 +150,6 @@ public class ApiManager {
         client.newCall(request).enqueue(callback);
     }
 
-    public void fetchRebalancingHistory(int cubeId, int page, ApiCallback callback) {
-        String params = "?count=20&page=" + page + "&cube_id=" + cubeId;
-        Request request = new Request.Builder()
-                .url(CUBES_REBALANCING_HISTORY + params)
-                .build();
-        client.newCall(request).enqueue(callback);
-    }
-
     /**
      * 获取股票简略信息
      *
@@ -203,9 +179,10 @@ public class ApiManager {
     public Response rebalancingCube(int cubeId, int cash, String holdings, String comment) {
         String params = String.format("cash=%d&comment=%s&cube_id=%d&holdings=%s&market=cn", cash, comment, cubeId, holdings);
         RequestBody body = RequestBody.create(MediaType.parse("text;charset=utf-8"), params);
-
+        //自动调仓账号是固定的，因此只能是QQ登录的账号Cookie
         Request request = new Request.Builder()
                 .url(CUBES_REBALANCING_CREATE)
+                .header("Cookie","xq_a_token=6a8cac4e9ea074035410b8ecea901a9053e2eaa6;u=9084578148")
                 .post(body)
                 .build();
         try {
@@ -222,17 +199,15 @@ public class ApiManager {
      * @return
      */
     public Response uploadImage(File file) {
-//        String params = String.format("status=%s&original=0&right=0&is_private=0", content);
-//        RequestBody body = RequestBody.create(MediaType.parse("text;charset=utf-8"),params);
         MediaType MEDIA_TYPE_PNG = MediaType.parse("image/png");
         RequestBody filebody = MultipartBody.create(MEDIA_TYPE_PNG, file);
         MultipartBody.Builder builder = new MultipartBody.Builder();
         builder.addFormDataPart("file", "file.png", filebody);
         builder.setType(MultipartBody.FORM);
 
-
         Request request = new Request.Builder()
                 .url(PHOTO_UPLOAD)
+                .header("Cookie","xq_a_token=6a8cac4e9ea074035410b8ecea901a9053e2eaa6;u=9084578148")
                 .post(builder.build())
                 .build();
         try {
@@ -255,6 +230,7 @@ public class ApiManager {
 
         Request request = new Request.Builder()
                 .url(STATUSES_UPDATE)
+                .header("Cookie","xq_a_token=6a8cac4e9ea074035410b8ecea901a9053e2eaa6;u=9084578148")
                 .post(body)
                 .build();
         try {
@@ -294,33 +270,6 @@ public class ApiManager {
             e.printStackTrace();
         }
         return searchResult;
-    }
-
-
-    public static void main(String[] args) {
-//        getInstance().cubeRankList(1);
-//        Response response = getInstance().searchCube("1738676");
-//        if(response.code()>=200 && response.code()<300){
-//            try {
-//                String res = response.body().string();
-//                System.out.println(res);
-//                Gson gson = new Gson();
-//                SearchResult<Cube> searchResult = gson.fromJson(res,new TypeToken<SearchResult<Cube>>() {
-//                }.getType());
-//                System.out.println(searchResult.getQ());
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-
-        for (int i = 0; i < 10; i++) {
-            getInstance().fetchRebalancingHistory(1773329, new ApiCallback() {
-                @Override
-                public void onSuccess(String response) {
-                }
-            });
-        }
-
     }
 
 }
---
+++ src/main/java/club/moredata/task/DingRobotTask.java
@@ -0,0 +1,49 @@
+package club.moredata.task;
+
+import com.dingtalk.api.DefaultDingTalkClient;
+import com.dingtalk.api.DingTalkClient;
+import com.dingtalk.api.request.OapiRobotSendRequest;
+import com.dingtalk.api.response.OapiRobotSendResponse;
+import com.taobao.api.ApiException;
+
+import java.util.Arrays;
+
+public class DingRobotTask {
+
+    public static void main(String[] args) {
+        DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/robot/send?access_token=09090e76e1b7727cd79ac3e9afc17ff953ae6f91e9d5de434b51a07ddd591171");
+        OapiRobotSendRequest request = new OapiRobotSendRequest();
+        request.setMsgtype("text");
+        OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
+        text.setContent("测试文本消息");
+        request.setText(text);
+        OapiRobotSendRequest.At at = new OapiRobotSendRequest.At();
+        at.setAtMobiles(Arrays.asList("13261303345"));
+        request.setAt(at);
+
+        request.setMsgtype("link");
+        OapiRobotSendRequest.Link link = new OapiRobotSendRequest.Link();
+        link.setMessageUrl("https://www.dingtalk.com/");
+        link.setPicUrl("");
+        link.setTitle("时代的火车向前开");
+        link.setText("这个即将发布的新版本，创始人陈航（花名“无招”）称它为“红树林”。\n" +
+                "而在此之前，每当面临重大升级，产品经理们都会取一个应景的代号，这一次，为什么是“红树林");
+        request.setLink(link);
+
+        request.setMsgtype("markdown");
+        OapiRobotSendRequest.Markdown markdown = new OapiRobotSendRequest.Markdown();
+        markdown.setTitle("杭州天气");
+        markdown.setText("#### 杭州天气 @156xxxx8827\n" +
+                "> 9度，西北风1级，空气良89，相对温度73%\n\n" +
+                "> ![screenshot](https://gw.alipayobjects.com/zos/skylark-tools/public/files/84111bbeba74743d2771ed4f062d1f25.png)\n"  +
+                "> ###### 10点20分发布 [天气](http://www.thinkpage.cn/) \n");
+        request.setMarkdown(markdown);
+        try {
+            OapiRobotSendResponse response = client.execute(request);
+            System.out.println(response.toString());
+
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
